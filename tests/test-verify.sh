950ae51d08448d07c94b12f43b76d8cf37637be7
Adam Dyson <contact@adamdyson.com.au>
2017-09-28 20:55:39 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Verify the entry is added to contab
-docker exec --tty ${container_id} env TERM=xterm crontab -u travis -l
+docker exec --tty ${container_id} env TERM=xterm crontab -u adam -l
