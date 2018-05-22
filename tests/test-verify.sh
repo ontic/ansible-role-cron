02d4c1f761fbb44f0beeb1a4a898a4110a2cef0e
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 22:44:56 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
 # Verify the entry is added to crontab.
 docker exec --tty ${container_id} env TERM=xterm crontab -u adam -l
